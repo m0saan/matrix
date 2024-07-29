36bf543435451887c7a775b8724e9b974b8c7042
m0saan <moboustt@student.1337.ma>
2024-07-29 17:49:41 +0100

    added trait implemtations of Add Sun amd Mul + Matrix

--- src/lib.rs
+++ src/lib.rs
@@ -66,10 +66,135 @@ where
             *elem = *elem * scalar;
         }
     }
+
+    pub fn dot(&self, v: Self) -> T {
+        let mut prod = T::default();
+        for (e1, e2) in self.data.iter().zip(v.data.iter()) {
+            prod = prod + (*e1 * *e2);
+        }
+        prod
+    }
 }
 
-// O(n)
-pub fn linear_combination<T>(vectors: &mut [Vector<T>], scalars: &[T]) -> Vector<T>
+impl<T> Add for Vector<T>
+where
+    T: Add<Output = T> + Default + Clone + Copy,
+{
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let mut result = Vector::from(&vec![T::default(); 0]);
+        for elem in self.data.iter().zip(rhs.data.iter()) {
+            result.data.push(*elem.0 + *elem.1)
+        }
+        result
+    }
+}
+
+impl<T> Sub for Vector<T>
+where
+    T: Sub<Output = T> + Default + Clone + Copy,
+{
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        let mut result = Vector::from(&vec![T::default(); 0]);
+        for elem in self.data.iter().zip(rhs.data.iter()) {
+            result.data.push(*elem.0 - *elem.1)
+        }
+        result
+    }
+}
+
+// Implement the Mul trait for Vector<T> with f32
+impl<T> Mul<f32> for Vector<T>
+where
+    T: Mul<f32, Output = T> + Clone,
+{
+    type Output = Self;
+
+    fn mul(self, scalar: f32) -> Self::Output {
+        let mut result = self.clone();
+        for element in result.data.iter_mut() {
+            *element = element.clone() * scalar;
+        }
+        result
+    }
+}
+
+use num::Signed; // 0.2.0
+
+impl<T> Vector<T>
+where
+    T: Add<Output = T>
+        + Sub<Output = T>
+        + Mul<Output = T>
+        + Div<Output = T>
+        + Copy
+        + Clone
+        + Default
+        + PartialOrd
+        + Signed,
+{
+    pub fn norm_1(&mut self) -> f32 {
+        let norm = T::default();
+        for e in self.data {
+            norm = norm + e.absolute();
+        }
+        norm
+    }
+
+    pub fn norm(&mut self) -> f32 {}
+
+    pub fn norm_inf(&mut self) -> f32 {}
+}
+
+//
+// Matrix
+//
+#[derive(Debug, Clone, Default, PartialEq, PartialOrd)]
+pub struct Matrix<T> {
+    pub data: Vec<Vec<T>>,
+}
+
+impl<T> std::fmt::Display for Matrix<T>
+where
+    T: Clone + Display + std::ops::AddAssign<T> + std::ops::SubAssign<T> + std::ops::MulAssign,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        for (i, row) in self.data.iter().enumerate() {
+            if i > 0 {
+                writeln!(f)?;
+            }
+            write!(f, "// [")?;
+            for (j, val) in row.iter().enumerate() {
+                if j > 0 {
+                    write!(f, ", ")?;
+                }
+                write!(f, "{:.1}", val)?;
+            }
+            write!(f, "]")?;
+        }
+        write!(f, "\n")?;
+        Ok(())
+    }
+}
+
+impl<T> Matrix<T>
+where
+    T: Default + Clone,
+{
+    pub fn from(data: &[&[T]]) -> Self {
+        let data = data.iter().map(|row| row.to_vec()).collect();
+        Self { data }
+    }
+
+    pub fn size(&self) -> (usize, usize) {
+        (self.data.len(), self.data[0].len())
+    }
+}
+
+impl<T> Matrix<T>
 where
     T: Add<Output = T>
         + Sub<Output = T>
@@ -80,36 +205,143 @@ where
         + Default
         + PartialOrd,
 {
-    assert_eq!(
-        vectors.len(),
-        scalars.len(),
-        "The number of vectors and scalars must be the same"
-    );
-
-    if vectors.is_empty() {
-        panic!("The number of vectors must be greater than 0");
+    pub fn add(&mut self, rhs: &Self) {
+        for (r_lhs, r_rhs) in self.data.iter_mut().zip(rhs.data.iter()) {
+            for (e_rhs, e_lhs) in r_lhs.iter_mut().zip(r_rhs.iter()) {
+                *e_rhs = *e_rhs + *e_lhs
+            }
+        }
     }
 
-    let mut result = Vector::from(&vec![T::default(); vectors[0].size()]);
-    for (vector, scalar) in vectors.iter_mut().zip(scalars.iter()) {
-        vector.scl(scalar.clone());
-        result.add(&vector);
+    pub fn sub(&mut self, rhs: &Self) {
+        for (r_lhs, r_rhs) in self.data.iter_mut().zip(rhs.data.iter()) {
+            for (e_rhs, e_lhs) in r_lhs.iter_mut().zip(r_rhs.iter()) {
+                *e_rhs = *e_rhs - *e_lhs
+            }
+        }
     }
-    result
+
+    pub fn scl(&mut self, scalar: T) {
+        for vec in self.data.iter_mut() {
+            for element in vec.iter_mut() {
+                *element = *element * scalar;
+            }
+        }
+    }
 }
 
-// pub fn lerp<V>(u: V, v: V, t: f32) -> V
+impl<T> Add for Matrix<T>
+where
+    T: Add<Output = T> + Default + Clone + Copy,
+{
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let mut result = self.clone();
+        for (l_vec, r_vec) in result.data.iter_mut().zip(rhs.data.iter()) {
+            for (e_lhs, e_rhs) in l_vec.iter_mut().zip(r_vec.iter()) {
+                *e_lhs = *e_lhs + *e_rhs;
+            }
+        }
+        result
+    }
+}
+
+impl<T> Sub for Matrix<T>
+where
+    T: Sub<Output = T> + Default + Clone + Copy,
+{
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        let mut result = self.clone();
+        for (l_vec, r_vec) in result.data.iter_mut().zip(rhs.data.iter()) {
+            for (e_lhs, e_rhs) in l_vec.iter_mut().zip(r_vec.iter()) {
+                *e_lhs = *e_lhs - *e_rhs;
+            }
+        }
+        result
+    }
+}
+
+// Implement the Mul trait for Vector<T> with f32
+impl<T> Mul<f32> for Matrix<T>
+where
+    T: Mul<f32, Output = T> + Clone,
+{
+    type Output = Self;
+
+    fn mul(self, scalar: f32) -> Self::Output {
+        let mut result = self.clone();
+        for vec in result.data.iter_mut() {
+            for element in vec.iter_mut() {
+                *element = element.clone() * scalar;
+            }
+        }
+        result
+    }
+}
+
+// O(n)
+// pub fn linear_combination<T>(vectors: &mut [Vector<T>], scalars: &[T]) -> Vector<T>
 // where
-//     V: Clone
-//         + std::ops::AddAssign<V>
-//         + std::ops::SubAssign<V>
-//         + std::ops::MulAssign<f32>
-//         + std::ops::Add<V, Output = V>
-//         + std::ops::Sub<V, Output = V>
-//         + std::ops::Mul<f32, Output = V>,
+//     T: Add<Output = T>
+//         + Sub<Output = T>
+//         + Mul<Output = T>
+//         + Div<Output = T>
+//         + Copy
+//         + Clone
+//         + Default
+//         + PartialOrd,
 // {
-//     let mut result = u;
-//     result *= 1.0 - t;
-//     result += v * t;
+//     assert_eq!(
+//         vectors.len(),
+//         scalars.len(),
+//         "The number of vectors and scalars must be the same"
+//     );
+
+//     if vectors.is_empty() {
+//         panic!("The number of vectors must be greater than 0");
+//     }
+
+//     let mut result = Vector::from(&vec![T::default(); vectors[0].size()]);
+//     for (vector, scalar) in vectors.iter().zip(scalars.iter()) {
+//         vector.scl(scalar.clone());
+//         result.add(vector);
+//     }
 //     result
 // }
+
+pub trait Lerp {
+    fn lerp(u: Self, v: Self, t: f32) -> Self;
+}
+
+impl Lerp for f32 {
+    fn lerp(u: Self, v: Self, t: Self) -> Self {
+        u + (v - u) * t
+    }
+}
+
+impl<T> Lerp for Vector<T>
+where
+    T: Add<Output = T> + Sub<Output = T> + Mul<f32, Output = T> + Clone + Default,
+{
+    fn lerp(u: Self, v: Self, t: f32) -> Self {
+        let mut result: Vector<T> = Vector {
+            data: vec![T::default(); u.size()],
+        };
+        for (e_u, e_v) in u.data.iter().zip(v.data.iter()) {
+            result
+                .data
+                .push(e_u.clone() + (e_v.clone() - e_u.clone()) * t)
+        }
+        result
+    }
+}
+
+pub fn lerp<V>(u: V, v: V, t: f32) -> V
+where
+    V: Add<Output = V> + Sub<Output = V> + Mul<f32, Output = V> + Clone + Default,
+{
+    u.clone() + (v - u) * t
+}
--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,92 @@
-use matrix::{Vector, linear_combination};
+use matrix::{Matrix, Vector};
 
 fn main() {
-    let mut u = Vector::from(&[2., 3.]);
-    let v = Vector::from(&[5., 7.]);
-    u.add(&v);
-    println!("{}", u);
-    // [7.0]
-    // [10.0]
-    //
+    // let mut u = Vector::from(&[2., 3.]);
+    // let v = Vector::from(&[5., 7.]);
+    // // u.add(&v);
+    // // println!("{}", u);
+    // // [7.0]
+    // // [10.0]
+    // //
 
-    let mut u = Vector::from(&[2., 3.]);
-    let v = Vector::from(&[5., 7.]);
-    u.sub(&v);
-    println!("{}", u);
-    // [-3.0]
-    // [-4.0]
+    // let o = u + v;
+    // println!("{}", o);
 
-    let mut u = Vector::from(&[2., 3.]);
-    u.scl(2.);
-    println!("{}", u);
-    // [4.0]
-    // [6.0]
+    // let mut u = Vector::from(&[2., 3.]);
+    // let v = Vector::from(&[5., 7.]);
+    // // u.sub(&v);
+    // // println!("{}", u);
+    // // [-3.0]
+    // // [-4.0]
+
+    // let o = u - v;
+    // println!("{}", o);
+
+    // let mut u = Vector::from(&[2., 3.]);
+    // // u.scl(2.);
+    // // println!("{}", u);
+    // // [4.0]
+    // // [6.0]
+
+    // let o = u * 2.0;
+    // println!("{}", o);
+
+    // Matrix addition
+    // let mut u = Matrix::from(&[&[1., 2.], &[3., 4.]]);
+    // let v = Matrix::from(&[&[7., 4.], &[-2., 2.]]);
+
+    // u.add(&v);
+    // let o = u + v;
+
+    // println!("{}", o);
+    // // [8.0, 6.0]
+    // // [1.0, 6.0]
+
+    // let mut u = Matrix::from(&[&[1., 2.], &[3., 4.]]);
+
+    // let v = Matrix::from(&[&[7., 4.], &[-2., 2.]]);
+
+    // u.sub(&v);
+    // let o = u - v;
+    // println!("{}", o);
+    // // [-6.0, -2.0]
+    // // [5.0, 2.0]
+
+    // let mut u = Matrix::from(&[&[1., 2.], &[3., 4.]]);
+
+    // u.scl(2.);
+    // let o = u * 2.0;
+
+    // println!("{}", o);
+    // // [2.0, 4.0]
+    // // [6.0, 8.0]
 
     // let mut u = Matrix::from(&[&[1, 2], &[3, 4]]);
 
     // u.scl(2);
 
     // println!("{}", u);
-    // // [2.0, 4.0]
-    // // [6.0, 8.0]
+    // [2.0, 4.0]
+    // [6.0, 8.0]
 
     // Linear Combination
-    let e1 = Vector::from(&[1., 0., 0.]);
-    let e2 = Vector::from(&[0., 1., 0.]);
-    let e3 = Vector::from(&[0., 0., 1.]);
+    // let e1 = Vector::from(&[1., 0., 0.]);
+    // let e2 = Vector::from(&[0., 1., 0.]);
+    // let e3 = Vector::from(&[0., 0., 1.]);
 
-    let v1 = Vector::from(&[1., 2., 3.]);
-    let v2 = Vector::from(&[0., 10., -100.]);
+    // let v1 = Vector::from(&[1., 2., 3.]);
+    // let v2 = Vector::from(&[0., 10., -100.]);
 
-    println!(
-        "{}",
-        linear_combination(&mut [e1, e2, e3], &[10., -2., 0.5])
-    );
+    // println!(
+    //     "{}",
+    //     linear_combination(&mut [e1, e2, e3], &[10., -2., 0.5])
+    // );
     // [10.]
     // [-2.]
     // [0.5]
 
-    println!("{}", linear_combination(&mut [v1, v2], &[10., -2.]));
-    // [10.][0.][230.]
-
+    // println!("{}", linear_combination(&mut [v1, v2], &[10., -2.]));
+    // // [10.][0.][230.]
 
     // println!("{}", lerp::<f32>(0., 1., 0.));
     // // 0.0
@@ -60,7 +98,7 @@ fn main() {
     // // 27.3
     // println!(
     //     "{}",
-    //     lerp(Vector::from([2., 1.]), Vector::from(4., 2.), 0.3)
+    //     lerp(Vector::from(&[2., 1.]), Vector::from(&[4., 2.]), 0.3)
     // );
     // [2.6]
     // [1.3]
@@ -68,4 +106,24 @@ fn main() {
     // 10.], [30., 40.]]), 0.5));
     // [[11., 5.5]
     // [16.5, 22.]]
+
+    // let mut u = Vector::from(&[0., 0.]);
+    // let v = Vector::from(&[1., 1.]);
+    // println!("{}", u.dot(v));
+    // // 0.0
+    // let mut u = Vector::from(&[1., 1.]);
+    // let v = Vector::from(&[1., 1.]);
+    // println!("{}", u.dot(v));
+    // // 2.0
+    // let mut u = Vector::from(&[-1., 6.]);
+    // let v = Vector::from(&[3., 2.]);
+    // println!("{}", u.dot(v));
+    // // 9.0
+    //
+
+    let u = Vector::from(&[0., 0., 0.]);
+    println!("{}", u.norm_1());
+
+    // println!("{}, {}, {}, {}", u.norm_1(), u.norm(), u.norm_inf());
+    // 0.0, 0.0, 0.0
 }
