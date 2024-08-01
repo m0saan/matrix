fb7e5dba30dbf039b54a53cde58029a34cb1b989
m0saan <moboustt@student.1337.ma>
2024-08-01 19:36:42 +0100

    refactored matrxi class

---
+++ src/matrix.rs
@@ -0,0 +1,148 @@
+use num::Num;
+use std::fmt::Display;
+
+use std::ops::{Add, AddAssign, Mul, MulAssign, Sub, SubAssign};
+use std::ops::{Deref, DerefMut, Index, IndexMut};
+
+#[derive(Debug, Clone)]
+pub struct Matrix<T, const M: usize, const N: usize> {
+    pub data: [[T; M]; N],
+}
+
+impl<T, const M: usize, const N: usize> Matrix<T, M, N>
+where
+    T: AddAssign + SubAssign + MulAssign + Copy,
+{
+    pub fn from(data: [[T; M]; N]) -> Self {
+        Self { data }
+    }
+
+    pub const fn size(&self) -> (usize, usize) {
+        (M, N)
+    }
+
+    pub fn add(&mut self, other: &Self) {
+        for (l_row, r_row) in self.data.iter_mut().zip(other.data.iter()) {
+            for (l, r) in l_row.iter_mut().zip(r_row.iter()) {
+                *l += *r;
+            }
+        }
+    }
+
+    pub fn sub(&mut self, other: &Self) {
+        for (l_row, r_row) in self.data.iter_mut().zip(other.data.iter()) {
+            for (l, r) in l_row.iter_mut().zip(r_row.iter()) {
+                *l -= *r;
+            }
+        }
+    }
+    pub fn scl(&mut self, scalar: T) {
+        for row in self.data.iter_mut() {
+            for elem in row.iter_mut() {
+                *elem *= scalar;
+            }
+        }
+    }
+}
+
+impl<T, const M: usize, const N: usize> IndexMut<(usize, usize)> for Matrix<T, M, N> {
+    fn index_mut(&mut self, index: (usize, usize)) -> &mut T {
+        &mut self.data[index.0][index.1]
+    }
+}
+
+impl<T, const M: usize, const N: usize> Index<(usize, usize)> for Matrix<T, M, N> {
+    type Output = T;
+
+    fn index(&self, index: (usize, usize)) -> &Self::Output {
+        &self.data.index(index.0).index(index.1)
+    }
+}
+
+impl<T, const M: usize, const N: usize> Deref for Matrix<T, M, N> {
+    type Target = [T; N];
+
+    fn deref(&self) -> &Self::Target {
+        todo!()
+    }
+}
+
+impl<T, const M: usize, const N: usize> DerefMut for Matrix<T, M, N> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        todo!()
+    }
+}
+
+impl<T, const M: usize, const N: usize> Add for Matrix<T, M, N>
+where
+    T: AddAssign + Copy + Num,
+{
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let mut result = self.clone();
+        for (l_row, r_row) in result.data.iter_mut().zip(rhs.data.iter()) {
+            for (l, r) in l_row.iter_mut().zip(r_row.iter()) {
+                *l += *r;
+            }
+        }
+        result
+    }
+}
+
+impl<T, const M: usize, const N: usize> Sub for Matrix<T, M, N>
+where
+    T: SubAssign + Copy + Num,
+{
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        let mut result = self.clone();
+        for (l_row, r_row) in result.data.iter_mut().zip(rhs.data.iter()) {
+            for (l, r) in l_row.iter_mut().zip(r_row.iter()) {
+                *l -= *r;
+            }
+        }
+        result
+    }
+}
+
+impl<T, const M: usize, const N: usize> Mul<T> for Matrix<T, M, N>
+where
+    T: MulAssign + Copy + Num,
+{
+    type Output = Self;
+
+    fn mul(self, rhs: T) -> Self::Output {
+        let mut result = self.clone();
+        for row in result.data.iter_mut() {
+            for elem in row.iter_mut() {
+                *elem *= rhs;
+            }
+        }
+        result
+    }
+}
+
+impl<T, const M: usize, const N: usize> Display for Matrix<T, M, N>
+where
+    T: AddAssign + SubAssign + MulAssign + Copy + std::fmt::Display,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        for (i, row) in self.data.iter().enumerate() {
+            if i > 0 {
+                writeln!(f)?;
+            }
+            write!(f, "// [")?;
+            for (j, val) in row.iter().enumerate() {
+                if j > 0 {
+                    write!(f, ", ")?;
+                }
+                write!(f, "{:.1}", val)?;
+            }
+            write!(f, "]")?;
+        }
+        write!(f, "\n")?;
+        Ok(())
+    }
+}
